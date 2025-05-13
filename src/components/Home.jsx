3b8685e3ca81097b4750c5d9d134dbc2258bca0d
ashiyaman <ashiya1005@gmail.com>
2025-05-13 11:42:47 +0530

    chore: Add Hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,22 +16,19 @@ const Home = () => {
     }, 100)
     return () => clearInterval(interval)
   }, [])
-  return (    
-      <div>        
-        <section>
-            <p>
-            <b>Hi, My Name is</b>{" "}
-            </p>
-            <h1>Ashiya Banu.</h1>
-            <h2 className="typing">{typingText}</h2>
-            <p>
-              I’m a MERN stack developer who loves turning ideas into fast,
-              functional, and beautiful web apps. Whether it’s crafting React UIs or
-              building powerful APIs.
-            </p>
-        </section>
-      </div>
-
+  return (           
+    <section className="my-4 px-5 md:my-8 lg:my-5">
+        <p className="text-slate-400 py-3 md:py-3 text-xl md:text-3xl lg:py-3">
+        <b>Hi, My Name is</b>{" "}
+        </p>
+        <h1 className="text-red-400 md:text-6xl py-3 md:py-3 text-4xl lg:py-3"><b>Ashiya Banu.</b></h1>
+        <h2 className="typing md:text-4xl text-cyan-300 py-3 lg:py-3 md:py-3 font-semibold text-2xl">{typingText}</h2>
+        <p className="text-slate-400 py-3 pr-10 md:py-8 lg:py-4 md:text-2xl text-xl">
+            I’m a MERN stack developer who loves turning ideas into fast,
+            functional, and beautiful web apps. Whether it’s crafting React UIs or
+            building powerful APIs.
+        </p>
+    </section>                
   );
 };
 
