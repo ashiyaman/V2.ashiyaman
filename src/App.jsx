da43f2dc9f78d00fa9fdd81032e2b082711d1f88
ashiyaman <ashiya1005@gmail.com>
2025-05-13 06:16:57 +0530

    chore: Initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='mx-auto bg-amber-200'>
+        Ashiya
+      </div>
+    </>
+  )
+}
+
+export default App
