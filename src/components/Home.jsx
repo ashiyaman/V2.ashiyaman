a182cf9007d27e36b3b12ffbd73edd31880a6792
ashiyaman <ashiya1005@gmail.com>
2025-05-14 18:47:34 +0530

    chore: Add view work btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,17 +17,32 @@ const Home = () => {
     return () => clearInterval(interval)
   }, [])
   return (           
-    <section className="my-4 px-5 md:my-10 lg:my-5">
-        <p className="text-slate-400 py-3 md:py-5 text-xl md:text-3xl lg:py-5">
+    <section className="py-4 px-5 md:py-10 bg-inherit">
+        <p className="text-slate-400 py-3 text-xl md:text-3xl">
         <b>Hi, My Name is</b>{" "}
         </p>
-        <h1 className="text-red-400 md:text-8xl py-3 md:py-8 lg:py-5 text-4xl"><b>Ashiya Banu.</b></h1>
-        <h2 className="typing md:text-4xl text-cyan-300 py-3 lg:py-6 md:py-8 font-semibold text-2xl">{typingText}</h2>
-        <p className="text-slate-400 py-3 pr-10 md:py-8 lg:py-5 text-xl">
+        <h1 className="text-red-400 md:text-6xl py-3 md:py-6 lg:py-1 text-4xl"><b>Ashiya Banu.</b></h1>
+        <h2 className="typing md:text-4xl text-cyan-300 py-3 lg:py-1 md:py-8 font-semibold text-2xl">{typingText}</h2>
+        <p className="text-slate-400 py-3 pr-10 md:pt-3 md:pb-6 text-xl">
             I’m a MERN stack developer who loves turning ideas into fast,
             functional, and beautiful web apps. Whether it’s crafting React UIs or
             building powerful APIs.
-        </p>
+        </p>        
+        
+        <a
+          href="#resume"
+          aria-label="See Ashiya's Projects"
+          className="inline-block group transition-all duration-300"
+        >
+          <div className="rounded-lg p-[2px] bg-gradient-to-r from-red-400 via-purple-500 to-cyan-400 group-hover:from-cyan-400 group-hover:via-purple-500 group-hover:to-red-400">
+            <div className="bg-slate-900 rounded-lg">
+              <button className="uppercase text-slate-300 font-semibold text-lg py-4 px-8 rounded-lg w-full transition-all duration-300 group-hover:text-slate-900">
+                View My Work
+              </button>
+            </div>
+          </div>
+        </a>
+
     </section>                
   );
 };
