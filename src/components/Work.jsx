123fdcc7821d5f56aaa81d515670f641e2db5631
ashiyaman <ashiya1005@gmail.com>
2025-05-28 19:33:27 +0530

    chore: Add projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -57,7 +57,7 @@ const Work = () => {
             </div>
         </div>
 
-      {/*  <div className="flex flex-col gap-6 py-6 w-full">
+      <div className="flex flex-col gap-6 py-6 w-full">
           <div className="mx-auto">
             <div className="rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-500 border border-slate-700">
               <div className="flex px-4 py-2 items-center gap-2">
@@ -100,7 +100,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-        </div>*/}
+        </div>
 
         <div className="flex flex-col gap-6 py-6 w-full">
           <div className="mx-auto">
